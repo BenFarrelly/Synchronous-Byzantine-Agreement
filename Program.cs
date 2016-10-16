7ce324d060878369be43f85885c2fa99ef1e9fbf
BenFarrelly <bfar697@aucklanduni.ac.nz>
2016-10-16 15:13:07 +1300

    Building tree correctly
    I hope

--- Program.cs
+++ Program.cs
@@ -19,16 +19,18 @@ namespace ByzantineAgreementSync
             // Private member-variables
             private T data;
              NodeList<T> neighbors = null;
+            public String id { get; set; }
             public List<Node<T>> children
             {
                 get; set;
             }
             Node<T> parent;
             public Node() { }
-            public Node(T data)
+            public Node(T data, String ID)
             {
                 children = new List<Node<T>>();
                 this.data = data;
+                this.id = ID;
             }
             public Node(Node<T> parent, T data)
             {
@@ -59,6 +61,7 @@ namespace ByzantineAgreementSync
                 child.parent = this;
                 
             }
+            
 
            
         }
@@ -83,14 +86,14 @@ namespace ByzantineAgreementSync
 
        
     }
-        public class Tree<T> : Node<T>
-        {
-            private Node<T> root;
-            public NodeList<T> children;
-            public Tree(T value)
-            {
-                root = new Node<T>(value);
-            }
+    //    public class Tree<T> : Node<T>
+    //    {
+     //       private Node<T> root;
+     //       public NodeList<T> children;
+     //       public Tree(T value)
+     //       {
+     //           root = new Node<T>(value);
+      //      }
           //  Node<T> getNode(int depth, )
            // {
                 //depth--;
@@ -100,7 +103,7 @@ namespace ByzantineAgreementSync
                // getNode(depth);
             //}
 
-        }
+        //}
     public class Message
         {
             public Message(int from, String value) { From = from; Value = value; }
@@ -138,7 +141,7 @@ namespace ByzantineAgreementSync
                 Init = init;
                 faulty = false;
                 script = null;
-                EIG = new Node<int>(Init);
+                EIG = new Node<int>(Init, "");
                 var temp = N;
                
             }
@@ -151,7 +154,7 @@ namespace ByzantineAgreementSync
                 script = ByzScript;
                 
                 var temp = N;
-                EIG = new Node<int>(Init);
+                EIG = new Node<int>(Init, "");
 
             }
             public int ID { get; private set; }
@@ -181,40 +184,171 @@ namespace ByzantineAgreementSync
                 }
                 return 70;
             }
-            public async Task ByzBody (MailboxProcessor<Message> inbox)
+            public String getLevel(int level, Node<int> node, String result)
             {
-                for(var i=0; i < N; i++)
-                {
-                    if(round == 1)
+                if (level != 1) {
+                    //Console.WriteLine("Before loop");
+                    
+                    foreach (Node<int> child in node.children)
                     {
-                        var msg = new Message(ID, Init+"");
-                        Byz[i].Post(msg);
-                        Console.WriteLine(round + " [" + ID + "]: Posting: " + msg.Value + " to " + (i + 1));
-                    }
-                    else
-                    {
-                        Console.WriteLine("WE HUR");
-                        var mes = getValue(EIG, 0);
-                        var msg = new Message(ID, "");
-                        Byz[i].Post(msg);
-                        Console.WriteLine(round + " [" + ID + "]: Posting: " + msg.Value + " to " + (i + 1));
+                       // Console.WriteLine("At loop: " + i++);
+                        result += getLevel(level-1, child, "");
                     }
                 }
-               // Console.WriteLine("BeforeBefore receive: " + ID + " ,  " );
+                else
+                {
+                    return node.Value.ToString();
+                }
+                return result;
+            }
+            public Node<int> GetLeaves(Node<int> parent)
+            {
+                if(parent.children.Count == 0)
+                {
+                    return parent;
+                }
+                else
+                {
+                    foreach(var child in parent.children)
+                    {
+                        GetLeaves(child);
+                    }
+                   
+                }
+                return null;
+            }
+            public void FindLeaves(Node<int> parent, List<Node<int>> leaves)
+            {
+                if (parent.children.Count != 0)
+                {
+                    foreach (var child in parent.children)
+                    {
+                       FindLeaves(child, leaves);
+                    }
+                }
+                else
+                {
+                    if (!leaves.Contains(parent))
+                    {
+                        leaves.Add(parent);
+                    }
+                }
+            }
+            public void GetLeavesFromSubTree(int ID, List<Node<int>> leaves)
+            {
+                
+            }
+
+
+
+            public async Task ByzBody (MailboxProcessor<Message> inbox)
+            {
+                
+                for(var i=0; i < N; i++)
+                {
+                    //    if(round == 1)
+                    //    {
+                    //var msg = new Message(ID, Init+"");
+                   // Console.WriteLine("Before getting level: " + i);
+                    var mess = String.Concat(getLevel(round, this.EIG, ""));
+                   
+
+                   // var mes = mess.Append(getLevel(round, this.EIG)).ToString();
+                   // Console.WriteLine("Message value: " + mess);
+                    var msg = new Message(ID, mess);
+                        Byz[i].Post(msg);
+                        Console.WriteLine(round + " [" + ID + "]: Posting: " + msg.Value + " to " + (i + 1));
+                  //  }
+                  //  else
+                  //  {
+                  //      Console.WriteLine("WE HUR");
+                  //      var mes = getValue(EIG, 0);
+                  //
+                  //     var msg = new Message(ID, "");
+                  //     Byz[i].Post(msg);
+                  //      Console.WriteLine(round + " [" + ID + "]: Posting: " + msg.Value + " to " + (i + 1));
+                  //  }
+                }
+                // Console.WriteLine("BeforeBefore receive: " + ID + " ,  " );
+                //Buffer for storing messages, ensureing tree is built in order
+                List<Message> buffer = new List<Message>();
+                var leaves = new List<Node<int>>();
+
+                FindLeaves(EIG, leaves);
+                var leavesQueue = new Queue<Node<int>>(leaves);
                 for (var i = 0; i < N; i++)
                 {
                     Console.WriteLine("Before receive: " + ID + " ,  " + i);
                     var msg = await inbox.Receive();
                     Console.WriteLine(round + ": Receiving[" + ID + "]: Value: " + msg.Value + ", From: " + msg.From);
                     var values = msg.Value.ToCharArray();
-                    var strValues = values.Select(x => x.ToString());
-                    foreach(var value in strValues)
-                    {
-                        var temp = Convert.ToInt32(value);
-                        this.EIG.AddChild(new Node<int>(temp));
-                       // Console.WriteLine("Child added");
-                       // Console.WriteLine("Successfully adding to tree");
+                    var receivedValues = values.Select(x => x.ToString()).ToList<String>();
+                    
+                    
+                     
+                   
+                    if (round == 1)
+                    { 
+                    foreach (var value in receivedValues)
+                        {
+                            var temp = Convert.ToInt32(value);
+                            this.EIG.AddChild(new Node<int>(temp, msg.From.ToString()));
+                            // Console.WriteLine("Child added");
+                            // Console.WriteLine("Successfully adding to tree");
+                        }
                     }
+                    else
+                    {
+                        //Need to remove the value that is from the node that is being stored
+                        // Each leaf should not get the value from the node they represent
+                        //E.G., if Node 1 is sending 0011 (where N=4), then the first leaf should
+                        //have 011.
+                        //If Node 3, sends 0011, then each third leaf will store 001
+                        var storedValues = receivedValues.ToArray();
+                        for (int j = msg.From-1; j < receivedValues.Count; j+=N)
+                        {
+                            storedValues[j] = null;
+                        }
+                        storedValues = storedValues.Where(x => x != null).ToArray<String>();
+                        //Now stored values does not include those from the ID
+                        // Console.WriteLine("Values to be stored: ");
+                        // foreach(var x in storedValues) { Console.WriteLine(x + " ,  "); }
+                        //problem with adding, for some reason not adding into final leaf
+                        //Change trying, (msg.From*(round-1)*(N-1)-> (msg.From*(round-1)*(N)
+
+                        //var leafQueue = new Queue<Node<int>>(leaves);
+                        //msg.Value is 0011 or something
+                        
+                        for(int j = 0; j < storedValues.Length; j++)
+                        {
+                            var leaf = leavesQueue.Dequeue();
+                            for(int k = 0; k < N-1; k++)
+                            {
+                                leaf.AddChild(new Node<int>(Convert.ToInt32(storedValues[j]), leaf.id +"."+msg.From));
+                            }
+                        }
+
+
+                        //var leaves = new List<Node<int>>();
+                        //leaves.Add(GetLeaves(EIG));
+                        // (int r = ((msg.From-1 * (round - 1) * (N)) - (N)); r < (msg.From * (round - 1) * (N)); r++)
+                        //
+
+                      /*  for (int r = ((msg.From * (round - 1) * (N)) - (N)); r < (msg.From * (round - 1) * (N)); r++)
+                        {
+                            for (int c = 0; c < N-1; c++)
+                            {
+                               
+                                leaves[r].AddChild(new Node<int>(Convert.ToInt32(storedValues[c]), leaves[r].id + "."+ c.ToString()));
+                               
+                                //Console.WriteLine("Child added at: r={0}, c={1}, stopping val:{2} ", r, c, (msg.From * (round - 1) * (N)));
+ 
+                            }
+                           // strValues.RemoveRange(0, N - 1);
+                        }*/
+                    
+                    }
+
                     //Console.WriteLine("The number of children in tree: " + this.EIG.children.Count );
                     //Console.WriteLine("Finished adding to tree");
                     //if (msg.From > 2) Console.WriteLine("3  and 4 are making it through");
@@ -246,29 +380,30 @@ namespace ByzantineAgreementSync
                 if (count == 0) Completion.SetResult(true);         
             }
 
-          /*  public async Task ByzBody2(MailboxProcessor<Message2> inbox)
-            {//Need to include options for faulty node
+            /*  public async Task ByzBody2(MailboxProcessor<Message2> inbox)
+              {//Need to include options for faulty node
+
+                  //begin round 2 message passing
+                  //int count2 = Interlocked.Decrement(ref CompletionCount);
+                  //Round 2, pass the first row of the EIG table to every other node.
+                  for (int i = 0; i < N; i++)
+                  {
+
+                      var msg2 = new Message2(ID, EIG[0]);
+                      Byz2[i].Post(msg2);
+                      Console.WriteLine("2: Posting: " + ID + ", " + EIG[0]);
+                  }
+                  for(int i = 0; i < N; i++)
+                  {
+                      var msg2 = await inbox.Receive();
+                      Console.WriteLine("2: Receiving[" + ID + "]: Value: " + msg2.Value + ", From: " + msg2.From);
+                      EIG[msg2.From] = msg2.Value;
+                  }
+                  var count2 = Interlocked.Decrement(ref CompletionCount);
+                  if (count2 == 0) Completion.SetResult(true);
+              }
+              */
             
-                //begin round 2 message passing
-                //int count2 = Interlocked.Decrement(ref CompletionCount);
-                //Round 2, pass the first row of the EIG table to every other node.
-                for (int i = 0; i < N; i++)
-                {
-                    
-                    var msg2 = new Message2(ID, EIG[0]);
-                    Byz2[i].Post(msg2);
-                    Console.WriteLine("2: Posting: " + ID + ", " + EIG[0]);
-                }
-                for(int i = 0; i < N; i++)
-                {
-                    var msg2 = await inbox.Receive();
-                    Console.WriteLine("2: Receiving[" + ID + "]: Value: " + msg2.Value + ", From: " + msg2.From);
-                    EIG[msg2.From] = msg2.Value;
-                }
-                var count2 = Interlocked.Decrement(ref CompletionCount);
-                if (count2 == 0) Completion.SetResult(true);
-            }
-            */
         }
 
     void Evaluation(ByzProcess[] generals)
@@ -330,9 +465,10 @@ namespace ByzantineAgreementSync
 
     static void Main(string[] args)
     {
-
+           
         if (args.Length == 1)
         {
+
             string path = args[0];
             string[] content = System.IO.File.ReadAllLines(path);
             ByzProcess[] generals = readGenerals(content);
